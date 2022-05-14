41d723b7307df3b6ad95991b452ecb99f7b42224
gejocastro <gejocastro@gmail.com>
2022-05-13 22:25:27 -0300

    Add files via upload

---
+++ constantes.hpp
@@ -0,0 +1,25 @@
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "TP-LINK_34C808";
+const char* password = "n163k25s";
+
+// Add your MQTT Broker IP address, example:
+const char* mqtt_server = "3.225.83.240";
+const int mqtt_port = 1883;
+
+// Add MQTT server credentials
+const char* mqtt_server_user = "user"; 
+const char* mqtt_server_password = "";//"password";
+
+
+// 
+int ledChannelFreq = 5000;
+int ledChannel = 0;
+int ledChannelResolution = 8;
+
+// Pin para la lectura de la entrada analògica del esp32
+const int AnalogPin = A0;
+
+// Pins para el control del LEDs
+const int ledPin = 21;
+const int ledPwmPin = 22;
